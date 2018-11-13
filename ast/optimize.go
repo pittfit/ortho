07d8a52c377d06197f3736d0cd2509069741a08a
Jordan Pittman <jordan@cryptica.me>
2018-11-13 16:24:33 -0500

    Extract a function

--- ast/optimize.go
+++ ast/optimize.go
@@ -4,31 +4,32 @@ package ast
 func (a *AST) Optimize() *AST {
 	root := a.root
 
-	// Walk through the AST
-	root = root.visit(func(n Node) Node {
-		if n.Type != TypeList && n.Type != TypeSequence {
-			return n
-		}
-
-		// List nodes can merge children of child list nodes
-		// Sequence nodes can merge children of child sequence nodes
-		children := []Node{}
-
-		for _, child := range n.Children {
-			if child.Type == n.Type {
-				children = append(children, child.Children...)
-			} else {
-				children = append(children, child)
-			}
-		}
-
-		n.Children = children
-
-		return n
-	})
+	root = root.visit(liftNestedNodes)
 
 	return &AST{
 		buf:  a.buf,
 		root: root,
 	}
+}
+
+func liftNestedNodes(n Node) Node {
+	if n.Type != TypeList && n.Type != TypeSequence {
+		return n
+	}
+
+	// List nodes can merge children of child list nodes
+	// Sequence nodes can merge children of child sequence nodes
+	children := []Node{}
+
+	for _, child := range n.Children {
+		if child.Type == n.Type {
+			children = append(children, child.Children...)
+		} else {
+			children = append(children, child)
+		}
+	}
+
+	n.Children = children
+
+	return n
 }
