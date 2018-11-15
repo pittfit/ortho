5257d3027bda4989ff2e51edea082a95fa135263
Jordan Pittman <jordan@cryptica.me>
2018-11-15 13:25:07 -0500

    Add token package

---
+++ token/token.go
@@ -0,0 +1,60 @@
+package token
+
+import "fmt"
+
+// TokenType
+type TokenType int
+
+// Currently only single byte runes are supported for these.
+const (
+	INVALID TokenType = iota
+	EOF
+	ESCAPE
+	LITERAL
+	WILDCARD
+	BRACE_OPEN
+	BRACE_CLOSE
+	LIST_SEPARATOR
+	RANGE_SEPARATOR
+	STEP_SEPARATOR
+)
+
+// Token
+type Token struct {
+	Type    TokenType
+	Literal []byte
+}
+
+func (t Token) String() string {
+	switch t.Type {
+	case LITERAL:
+		return fmt.Sprintf("%v(%v)", t.Type.String(), t.Literal)
+	default:
+		return t.Type.String()
+	}
+}
+
+func (t TokenType) String() string {
+	switch t {
+	case INVALID:
+		return "INVALID"
+	case EOF:
+		return "EOF"
+	case LITERAL:
+		return "LITERAL"
+	case WILDCARD:
+		return "WILDCARD"
+	case BRACE_OPEN:
+		return "BRACE_OPEN"
+	case BRACE_CLOSE:
+		return "BRACE_CLOSE"
+	case LIST_SEPARATOR:
+		return "LIST_SEPARATOR"
+	case RANGE_SEPARATOR:
+		return "RANGE_SEPARATOR"
+	case STEP_SEPARATOR:
+		return "STEP_SEPARATOR"
+	default:
+		return "UNKNOWN"
+	}
+}
